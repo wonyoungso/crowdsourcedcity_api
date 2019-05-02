c51c020b2f23e8c234b865f8e92e81be6cc9eb0e
Wonyoung So <wso@mit.edu>
2019-05-02 01:49:35 -0400

    udpate

--- app/controllers/admin/devices_scanners_controller.rb
+++ app/controllers/admin/devices_scanners_controller.rb
@@ -2,17 +2,17 @@ class Admin::DevicesScannersController < Admin::AdminController
   def index
 
     if params[:timestamp].present?
-      @timestamp = DateTime.parse(params[:timestamp]).in_time_zone("Eastern Time (US & Canada)").to_i * 1000
-    elsif DevicesScanner.first.present?
-      @timestamp = DevicesScanner.first.timestamp_integer
+      @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
+    elsif DevicesScanner.last.present?
+      @timestamp = DevicesScanner.last.timestamp_integer
     else
       @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
     end
 
-    timestamp_start = @timestamp - 40000
-    timestamp_end = @timestamp + 40000
+    timestamp_start = @timestamp - 20000
+    timestamp_end = @timestamp + 20000
 
-    @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.where(timestamp_integer: timestamp_start...timestamp_end), @timestamp)
+    @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device).where('devices.device_type' => "wifi").or(DevicesScanner.joins(:device).where('devices.is_furniture' => false)).where(:timestamp_integer => timestamp_start...timestamp_end), @timestamp)
 
   end
 end
--- app/controllers/api/devices_scanners_controller.rb
+++ app/controllers/api/devices_scanners_controller.rb
@@ -2,16 +2,16 @@ class Api::DevicesScannersController < ApplicationController
   def index
 
     if params[:timestamp].present?
-      @timestamp = DateTime.parse(params[:timestamp]).to_i * 1000
+      @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
     elsif DevicesScanner.first.present?
       @timestamp = DevicesScanner.first.timestamp_integer
     else
       @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
     end
-    timestamp_start = @timestamp - 10000
-    timestamp_end = @timestamp + 10000
+    timestamp_start = @timestamp - 20000
+    timestamp_end = @timestamp + 20000
 
-    @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.where(timestamp_integer: timestamp_start...timestamp_end), @timestamp)
+    @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device).where('devices.device_type' => "wifi").or(DevicesScanner.joins(:device).where('devices.is_furniture' => false)).where(:timestamp_integer => timestamp_start...timestamp_end), @timestamp)
 
     render json: {success: true, result: @result}
   end
@@ -20,15 +20,15 @@ class Api::DevicesScannersController < ApplicationController
 
 
     if params[:timestamp].present?
-      @timestamp = DateTime.parse(params[:timestamp]).to_i * 1000
+      @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
     elsif DevicesScanner.first.present?
       @timestamp = DevicesScanner.first.timestamp_integer
     else
       @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
     end
 
-    timestamp_start = @timestamp - 10000
-    timestamp_end = @timestamp + 10000
+    timestamp_start = @timestamp - 20000
+    timestamp_end = @timestamp + 20000
     @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device, :scanner).where('devices.device_type' => "wifi", 'devices.is_furniture' => false, :timestamp_integer => timestamp_start...timestamp_end), @timestamp)
 
     render json: {success: true, result: @result}
@@ -36,15 +36,15 @@ class Api::DevicesScannersController < ApplicationController
 
   def furniture
     if params[:timestamp].present?
-      @timestamp = DateTime.parse(params[:timestamp]).to_i * 1000
+      @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
     elsif DevicesScanner.first.present?
       @timestamp = DevicesScanner.first.timestamp_integer
     else
       @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
     end
 
-    timestamp_start = @timestamp - 40000
-    timestamp_end = @timestamp + 40000
+    timestamp_start = @timestamp - 20000
+    timestamp_end = @timestamp + 20000
     @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device, :scanner).where('devices.device_type' => "bluetooth", 'devices.is_furniture' => true, :timestamp_integer => timestamp_start...timestamp_end), @timestamp)
 
     render json: {success: true, result: @result}
