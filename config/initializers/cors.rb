a2de21b1b5db314705616811bf1671d18e608553
Wonyoung So <mail@wonyoung.so>
2019-05-05 20:26:55 -0400

    update

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    resource '*',
+      headers: :any,
+      methods: %i(get post put patch delete options head)
+  end
+end
