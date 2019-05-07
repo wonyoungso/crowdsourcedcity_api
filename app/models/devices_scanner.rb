d71704316e209d199ecbf9bd5508850a97e17b50
Wonyoung So <mail@wonyoung.so>
2019-05-07 10:44:10 -0400

    update

--- app/models/devices_scanner.rb
+++ app/models/devices_scanner.rb
@@ -22,7 +22,7 @@ class DevicesScanner < ApplicationRecord
         device_data = {
           id: device.id,
           type: device.device_type,
-          # mac_address: device.mac_address,
+          mac_address: device.mac_address,
           is_furniture: device.is_furniture,
           signal_strengths: []
         }
