01beabce8051af6af064c89204d19f1b646f6341
Wonyoung So <mail@wonyoung.so>
2019-05-09 09:44:07 -0400

    update

--- app/controllers/scanners_controller.rb
+++ app/controllers/scanners_controller.rb
@@ -8,7 +8,9 @@ class ScannersController < ApplicationController
     @scanner = Scanner.find_or_create_by(device_id: device_data[:d])
     @scanner.update_column(:updated_at, DateTime.now)
 
-    ScannerParseJob.perform_later(device_data, request.remote_ip)
+    Scanner.parse_data(device_data, remote_ip)
+
+    # ScannerParseJob.perform_later(device_data, request.remote_ip)
     
     render json: {
       "message": "posted data",
