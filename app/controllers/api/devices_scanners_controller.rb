12bd23dd7c347463289ac8c038e1494e157a1b11
Wonyoung So <mail@wonyoung.so>
2019-05-10 16:40:28 -0400

    update

--- app/controllers/api/devices_scanners_controller.rb
+++ app/controllers/api/devices_scanners_controller.rb
@@ -1,19 +1,19 @@
 class Api::DevicesScannersController < ApplicationController
   def index
-    render json: {success: true, result: []}
-    # if params[:timestamp].present?
-    #   @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
-    # elsif DevicesScanner.first.present?
-    #   @timestamp = DevicesScanner.first.timestamp_integer
-    # else
-    #   @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
-    # end
-    # timestamp_start = @timestamp - 20000
-    # timestamp_end = @timestamp + 20000
+    # render json: {success: true, result: []}
+    if params[:timestamp].present?
+      @timestamp = DateTime.parse(params[:timestamp]).change(:offset => "-0400").to_i * 1000
+    elsif DevicesScanner.first.present?
+      @timestamp = DevicesScanner.first.timestamp_integer
+    else
+      @timestamp = DateTime.parse("2019-04-16 14:31:33").to_i * 1000
+    end
+    timestamp_start = @timestamp - 20000
+    timestamp_end = @timestamp + 20000
 
-    # @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device).where('devices.device_type' => "wifi").or(DevicesScanner.joins(:device).where('devices.is_furniture' => true)).where(:timestamp_integer => timestamp_start...timestamp_end), @timestamp)
+    @result = DevicesScanner.find_all_devices_around_time(DevicesScanner.joins(:device).where('devices.device_type' => "wifi").or(DevicesScanner.joins(:device).where('devices.is_furniture' => true)).where(:timestamp_integer => timestamp_start...timestamp_end), @timestamp)
 
-    # render json: {success: true, result: @result}
+    render json: {success: true, result: @result}
   end
 
   def wifi 
